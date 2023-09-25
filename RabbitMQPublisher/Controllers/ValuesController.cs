c7bdca611d39df87c674dd0d86dab2de6e9eeb09
uniquser <murat.karasah@madamecoco.com>
2023-09-25 14:25:19 +0300

    Add project files.

---
+++ RabbitMQPublisher/Controllers/ValuesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RabbitMQPublisher.Factory;
+
+namespace RabbitMQPublisher.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        RabbitMQService? rabbitMQ;
+        [HttpPost("addproduct")]
+        public IActionResult AddProduct(string product)
+        {
+            rabbitMQ = new RabbitMQService();
+            rabbitMQ.GetRabbitMQConnection(product);
+
+            return Ok();
+        }
+    }
+}
---
+++ RabbitMQPublisher/Factory/RabbitMQService.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace RabbitMQPublisher.Factory
+{
+    public class RabbitMQService
+    {
+        // localhost üzerinde kurulu olduğu için host adresi olarak bunu kullanıyorum.
+        private readonly string _hostName = "localhost";
+
+        public void GetRabbitMQConnection<T>(T message)
+        {
+            //Here we specify the Rabbit MQ Server. we use rabbitmq docker image and use it
+            var factory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                UserName = "admin",
+                Password="123456"
+            };
+            //Create the RabbitMQ connection using connection factory details as i mentioned above
+            var connection = factory.CreateConnection();
+            //Here we create channel with session and model
+            using var channel = connection.CreateModel();
+            //declare the queue after mentioning name and a few property related to that
+            channel.QueueDeclare("product", exclusive: false);
+            //Serialize the message
+            var json = JsonConvert.SerializeObject(message);
+            var body = Encoding.UTF8.GetBytes(json);
+            //put the data on to the product queue
+            channel.BasicPublish(exchange: "", routingKey: "product", body: body);
+        }
+    }
+}
